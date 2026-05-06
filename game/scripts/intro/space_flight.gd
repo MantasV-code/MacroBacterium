5c59a42959d2b7f3e5a16ea19189f0b51d122e25
Killian Condon Smithson <killiancondon2191@gmail.com>
2026-05-06 12:24:26 +0100

    update space flight

--- game/scripts/intro/space_flight.gd
+++ game/scripts/intro/space_flight.gd
@@ -6,10 +6,6 @@ func _ready() -> void:
 	$fly.play("fly")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_fly_animation_finished(anim_name: StringName) -> void:
 	fade_out()
